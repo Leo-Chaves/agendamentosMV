9b471786c76001b8c7cf6243e58a2a3c4f724b39
leo-chaves <leonardo.2021107241@unicap.br>
2026-06-11 13:31:16 -0300

    main init

---
+++ src/test/java/com/projeto/agendamentosMV/AgendamentosMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.agendamentosMV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
